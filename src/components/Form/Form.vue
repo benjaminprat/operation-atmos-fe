528d14e0063c3e3baadf8b6703f47e1a2b6a45be
Ben Prat <bvprat@gmail.com>
2021-05-14 00:17:34 -0600

    Make code spacing consistent

--- src/components/Form/Form.vue
+++ src/components/Form/Form.vue
@@ -1,26 +1,47 @@
 <template>
   <form class="form">
-    <input class="button" type="submit" name="nearestLocation" value="Nearest Location" v-on:click.prevent="findNearestLocation">
-    <label for="states">States:</label>
-    <select class="dropdown" id="dropdown-states" @change="selectState">
+    <input
+      class="button"
+      type="submit"
+      name="nearestLocation"
+      value="Nearest Location"
+      v-on:click.prevent="findNearestLocation">
+    <select
+      class="dropdown"
+      id="dropdown-states"
+      @change="selectState"
+      aria-label="dropdown to select a state">
       <option selected disabled>Choose a State</option>
       <option
         v-for="(state, index) in states"
         :key="index"
-        :value="state.state"
-      >{{state.state}}</option>
+        :value="state.state">
+        {{state.state}}
+      </option>
     </select>
-    <label for="cities">Cities:</label>
-    <select class="dropdown" id="dropdown-cities" @change="selectCity" disabled>
+    <select
+      class="dropdown"
+      id="dropdown-cities"
+      @change="selectCity"
+      aria-label="dropdown to select a city"
+      disabled>
       <option selected disabled>Choose a City</option>
       <option
         v-for="(city, index) in cities"
         :key="index"
-        :value="city.city"
-      >{{city.city}}</option>
+        :value="city.city">
+        {{city.city}}
+      </option>
     </select>
-    <p>{{ dropdownErrorMessage }}</p>
-    <input class="button" type="submit" name="submit" value="Submit" id="submit" v-on:click.prevent="submitLocation" disabled>
+    <p class="error-message">{{ dropdownErrorMessage }}</p>
+    <input
+      class="button"
+      id="submit"
+      type="submit"
+      name="submit"
+      value="Submit"
+      v-on:click.prevent="submitLocation"
+      disabled>
   </form>
 </template>
 
