c22c4db0e8fbe9bc4bd04aac8b735ea9396b1697
Ben Prat <bvprat@gmail.com>
2021-05-13 19:53:06 -0600

    Populate cities dropdown from API

--- src/fetchAPI.js
+++ src/fetchAPI.js
@@ -1,17 +1,17 @@
 export default {
-  getStates() {
+  getStates () {
     return fetch('http://api.airvisual.com/v2/states?country=USA&key=f5bb78f7-2da9-4639-9522-6e0786c72a31')
       .then(response => response.json())
       .catch(error => console.log(error))
   },
 
-  getCities(state) {
+  getCities (state) {
     return fetch(`http://api.airvisual.com/v2/cities?state=${state}&country=USA&key=f5bb78f7-2da9-4639-9522-6e0786c72a31`)
       .then(response => response.json())
       .catch(error => console.log(error))
   },
 
-  getLocale(city, state) {
+  getLocale (city, state) {
     return fetch(`http://api.airvisual.com/v2/city?city=${city}&state=${state}&country=USA&key=f5bb78f7-2da9-4639-9522-6e0786c72a31`)
       .then(response => response.json())
       .catch(error => console.log(error))
