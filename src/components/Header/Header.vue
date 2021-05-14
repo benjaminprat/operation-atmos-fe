1444e5c145ab90f942cf35c617e140764fc23afe
Ben Prat <bvprat@gmail.com>
2021-05-14 00:18:56 -0600

    Edit for readability

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,14 +1,10 @@
 <template>
   <header>
-    <h1>Operation Atmos</h1>
-    <h2>Find Fresh Air for your outdoor activities</h2>
+    <h1>ActivAir</h1>
+    <h2>Find fresh air for your outdoor activities.</h2>
   </header>
 </template>
 
-<script>
-export default {
-  name: 'Header'
-}
-</script>
+<script>export default { name: 'Header' }</script>
 
 <style src='./Header.css'></style>
