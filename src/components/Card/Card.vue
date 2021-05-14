8c3b107c9f5500439f949c743379537275613b52
Ben Prat <bvprat@gmail.com>
2021-05-13 19:03:23 -0600

    Add initial styling to Card and Container components

--- src/components/Card/Card.vue
+++ src/components/Card/Card.vue
@@ -1,6 +1,9 @@
 <template>
-  <article>
-    <h2>Operation Atmos</h2>
+  <article class="location-card">
+    <h2>Denver, CO</h2>
+    <!-- <img src="../../assets/01d.png" class="weather-icon"> -->
+    <h3>Weather</h3>
+    <h3>AirQ</h3>
   </article>
 </template>
 
